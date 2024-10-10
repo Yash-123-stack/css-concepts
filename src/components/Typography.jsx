ab26e92160daf8d8e0b6f84b1d0759d177b57677
Yash-123-stack <yash.kulshrestha12@gmail.com>
2024-10-10 23:38:59 +0530

    added a dot in typography comp

--- src/components/Typography.jsx
+++ src/components/Typography.jsx
@@ -5,7 +5,7 @@ export default function Typography() {
   return (
     <>
      <div className ="typography-class1">This is a Typography concept
-      <h3>This is my typography class</h3>
+      <h3>This is my typography class.</h3>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, modi eum. Tenetur blanditiis quae laudantium cupiditate temporibus. Ex, architecto ut est corporis voluptates expedita qui nesciunt atque dolores ullam!</p>
       <a href="#">Click me!</a>
      </div>
